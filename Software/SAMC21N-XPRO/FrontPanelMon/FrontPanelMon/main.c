4b0431b6b442a1fe18cb026badc3c50678355ef2
Jac Goudsmit <jac.goudsmit@gmail.com>
2019-05-28 00:16:42 -0700

    Added code to dump data from SPI to debug console
    Tested with a connection to a SPI test transmitter made with an Arduino Micro.

--- Software/SAMC21N-XPRO/FrontPanelMon/FrontPanelMon/main.c
+++ Software/SAMC21N-XPRO/FrontPanelMon/FrontPanelMon/main.c
@@ -14,15 +14,46 @@
 #include <string.h>
 #include <stdio.h>
 
+#if 0
+volatile bool gotdata;
+
+static void complete_cb_SPI_EXT1(const struct spi_s_async_descriptor *const desc)
+{
+  /* Transfer completed */
+  gotdata = true;
+}
+#endif
+
 int main(void)
 {
   atmel_start_init();
 
+  struct io_descriptor *io;
+  spi_s_async_get_io_descriptor(&SPI_EXT1, &io);
+
+//  spi_s_async_register_callback(&SPI_EXT1, SPI_S_CB_RX, (FUNC_PTR)complete_cb_SPI_EXT1);
+  spi_s_async_enable(&SPI_EXT1);
+
+  printf("Hello Printf\n");
+  fflush(stdout);
+
+  uint count = 0;
+  
   for(;;)
   {
-    printf("Hello Printf\n");
-    fflush(stdout);
-
-    delay_ms(1000);
+    uint8_t buf;
+    
+    while (io_read(io, &buf, 1))
+    {
+      printf("%02X ", buf & 0xFF);
+      
+      if (++count == 16)
+      {
+        printf("\n");
+        count = 0;
+      }
+    }
+    
+//    delay_ms(1);
   }  
 }
